6ddd2671aa6d580a2fc8557fe5b818d14db9c371
bluelightspirit <bluelittlebears@hotmail.com>
2024-11-27 20:00:05 -0500

    added getters/setters for trader

--- ConsoleApplication1/Trader.cpp
+++ ConsoleApplication1/Trader.cpp
@@ -1,1 +1,31 @@
 #include "Trader.h"
+
+string Trader::get_name()
+{
+	return name;
+}
+
+double Trader::get_balance()
+{
+	return balance;
+}
+
+vector<string> Trader::get_portfolio()
+{
+	return portfolio;
+}
+
+void Trader::set_name(string given_name)
+{
+	name = given_name;
+}
+
+void Trader::set_balance(double given_balance)
+{
+	balance = given_balance;
+}
+
+void Trader::set_portfolio(vector<string> given_portfolio)
+{
+	portfolio = given_portfolio;
+}
--- ConsoleApplication1/Trader.h
+++ ConsoleApplication1/Trader.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include <vector>
+using namespace std;
 class Trader
 {
+public:
+// getters
+	string get_name();
+	double get_balance();
+	vector<string> get_portfolio();
+// setters
+	void set_name(string given_name);
+	void set_balance(double given_balance);
+	void set_portfolio(vector<string> given_portfolio);
+	
+private:
+	string name;
+	double balance;
+	vector<string> portfolio;
 };
 
