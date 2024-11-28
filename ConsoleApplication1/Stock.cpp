b250883f42059bfd6dfe6ec847a4019408fb7178
bluelightspirit <bluelittlebears@hotmail.com>
2024-11-27 20:29:54 -0500

    added getters setters for stock

--- ConsoleApplication1/Stock.cpp
+++ ConsoleApplication1/Stock.cpp
@@ -1,1 +1,31 @@
 #include "Stock.h"
+
+string Stock::get_symbol()
+{
+	return symbol;
+}
+
+double Stock::get_price()
+{
+	return price;
+}
+
+unordered_map<string, int> Stock::get_attributes()
+{
+	return attributes;
+}
+
+void Stock::set_symbol(string given_symbol)
+{
+	symbol = given_symbol;
+}
+
+void Stock::set_price(double given_price)
+{
+	price = given_price;
+}
+
+void Stock::set_attributes(unordered_map<string, int> given_attributes)
+{
+	attributes = given_attributes;
+}
--- ConsoleApplication1/Stock.h
+++ ConsoleApplication1/Stock.h
@@ -1,6 +1,23 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 class Stock
 {
+public:
+	// getters
+	string get_symbol();
+	double get_price();
+	unordered_map<string, int> get_attributes();
+	// setters
+	void set_symbol(string given_symbol);
+	void set_price(double given_price);
+	void set_attributes(unordered_map<string, int> given_attributes);
 
+private:
+	string symbol;
+	double price;
+	unordered_map<string, int> attributes;
 };
 
